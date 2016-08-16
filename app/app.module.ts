1cd2ed9548a94905c7b082e59384b6fbebf9e56f
Thoi Duong <dnhthoi@gmail.com>
2016-08-16 16:24:02 +0700

    angular http implementions

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,5 +1,12 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
+// Imports for loading & configuring the in-memory web api
+import { HttpModule, XHRBackend } from '@angular/http';
+import { InMemoryBackendService, SEED_DATA } from 'angular2-in-memory-web-api';
+import { InMemoryDataService }               from './in-memory-data.service';
+import { HeroSearchComponent } from './hero-search.component';
+
 import { FormsModule }   from '@angular/forms';
 
 import { AppComponent }  from './app.component';
@@ -12,9 +19,13 @@ import { DashboardComponent } from './dashboard.component';
 
 
 @NgModule({
-  imports: [ BrowserModule, FormsModule, routing ],
-  declarations: [ HeroDetailComponent, DashboardComponent, HeroesComponent, AppComponent ],
-  providers: [HeroService],
+  imports: [ BrowserModule, FormsModule, routing, HttpModule ],
+  declarations: [HeroSearchComponent, HeroDetailComponent, DashboardComponent, HeroesComponent, AppComponent ],
+  providers: [
+    HeroService,
+    { provide: XHRBackend, useClass: InMemoryBackendService }, // in-mem server
+    { provide: SEED_DATA,  useClass: InMemoryDataService }     // in-mem server data
+  ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -10,6 +10,7 @@ import { HeroService }  from './hero.service';
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
   selectedHero: Hero;
+  addingHero = false;
   constructor( private router: Router, private heroService: HeroService) {}
   getHeroes() {
     this.heroService.getHeroes().then(heroes => this.heroes = heroes);
@@ -20,5 +21,25 @@ export class HeroesComponent implements OnInit {
   onSelect(hero: Hero) { this.selectedHero = hero; }
   gotoDetail() {
     this.router.navigate(['/detail', this.selectedHero.id]);
+  }
+  addHero() {
+    this.addingHero = true;
+    this.selectedHero = null;
+  }
+
+  close(savedHero: Hero) {
+    this.addingHero = false;
+    if (savedHero) { this.getHeroes(); }
+  }
+
+  deleteHero(hero: Hero, event: any) {
+    event.stopPropagation();
+    this.heroService
+        .delete(hero)
+        .then(res => {
+          this.heroes = this.heroes.filter(h => h !== hero);
+          if (this.selectedHero === hero) { this.selectedHero = null; }
+        })
+        .catch(error => this.error = error);
   }
 }
